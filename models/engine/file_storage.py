e027af559d9210a0ae1750c02a5be28cfeb919ef
Slonjoh <tobson11122@gmail.com>
2023-12-08 18:14:51 +0000

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from os.path import isfile
 import sys
 import os
+import models
 from models.base_model import BaseModel
 
 
@@ -30,28 +31,29 @@ class FileStorage:
         """Serializes __objects to the JSON file."""
         serialized_objects = {}
         for key, obj in FileStorage.__objects.items():
-            class_name = obj.__class__.__name__
-            obj_id = obj.id
-            new_key = "{}.{}".format(class_name, obj_id)
-            serialized_objects[new_key] = obj.to_dict()
+            if not isinstance(obj, dict):
+                serialized_objects[key] = obj.to_dict()
+            else:
+                serialized_objects[key] = obj
+                serialized_objects[key]['created_at'] = str(obj['created_at'])
+                serialized_objects[key]['updated_at'] = str(obj['updated_at'])
 
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
-        if os.path.isfile(FileStorage.__file_path):
+        try:
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
-                try:
-                    obj_dict = json.load(file)
-                except ValueError:
-                    obj_dict = {}
-
+                obj_dict = json.load(file)
             for key, value in obj_dict.items():
-                class_name, obj_id = key.split('.')
-                obj = eval(class_name)(**value)
-                FileStorage.__objects[key] = obj
+                class_name = value['__class__']
+                del value['__class__']
+                obj_instance = eval(class_name)(**value)
+                self.new(obj_instance)
+        except FileNotFoundError:
+            return
 
 
-storage = FileStorage()
-storage.reload()
+# storage = FileStorage()
+# storage.reload()
