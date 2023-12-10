6603b9885fdf1c209b1292a4891e009cf645cc1f
Slonjoh <tobson11122@gmail.com>
2023-12-09 16:41:45 -0800

    progress

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,11 +23,6 @@ class FileStorage:
     """
     FileStorage class.
     """
-    CLASSES = {
-        'BaseModel': BaseModel,
-        'User': User,
-    }
-
     __file_path = "file.json"
     __objects = {}
 
@@ -39,47 +34,28 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file."""
         serialized_objects = {}
-        for key, obj in FileStorage.__objects.items():
-            if not isinstance(obj, dict):
-                serialized_objects[key] = obj.to_dict()
-            else:
-                serialized_objects[key] = obj
-                serialized_objects[key]['created_at'] = str(obj['created_at'])
-                serialized_objects[key]['updated_at'] = str(obj['updated_at'])
-
+        for key, value in FileStorage.__objects.items():
+            serialized_objects[key] = value.to_dict()
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """
+        Deserializes the JSON file to __object.
+        """
+        dict_class = {"Amenity": Amenity, "BaseModel": BaseModel,
+                      "City": City, "Place": Place, "Review": Review,
+                      "State": State, "User": User}
         try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
-                obj_dict = json.load(file)
-            for key, value in obj_dict.items():
-                class_name = value['__class__']
-                del value['__class__']
-                class_obj = self.get_class_by_name(class_name)
-                obj_instance = class_obj(**value)
-                self.new(obj_instance)
+            with open(FileStorage.__file_path, 'r' encoding='utf-8') as file:
+                data_file = json.load(file)
+                for key, value in data_file.items():
+                    self.new(dict_class[value['__class__']](**value))
         except FileNotFoundError:
-            return
-
-    def get_class_by_name(self, class_name):
-        """Get class by its name."""
-        return getattr(models, class_name, BaseModel)
-
-    def classes(self):
-        """Returns a list of available classes."""
-        return [class_obj.__name__ for class_obj in models.__dict__.values()
-                if type(class_obj) == type]
-
-
-if __name__ == "__main__":
-    storage = FileStorage()
-    storage.reload()
+            pass
