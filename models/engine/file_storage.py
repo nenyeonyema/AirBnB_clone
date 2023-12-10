19522e1cbc1ce12d767515108b341ce3ec8db5bd
Slonjoh <tobson11122@gmail.com>
2023-12-09 16:11:05 -0800

    import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,12 @@ import os
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
