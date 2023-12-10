032714c53882476303f2f29a9ca50a378a255611
Slonjoh <tobson11122@gmail.com>
2023-12-09 19:08:03 -0800

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,90 +1,60 @@
 #!/usr/bin/python3
 """
-Unittests for BaseModel class.
+Unit tests for BaseModel class.
 """
 
-import os
-import json
-import models
 import unittest
-import sys
-from datetime import datetime
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     """
-    Test cases for BaseModel class.
+    Test cases for the BaseModel class.
     """
 
-    def test_create_instance(self):
+    def test_init(self):
         """
-        Test creating an instance of BaseModel.
+        Test the initialization of the BaseModel.
         """
         my_model = BaseModel()
         self.assertIsInstance(my_model, BaseModel)
-
-    def test_attributes(self):
-        """
-        Test the attributes of BaseModel.
-        """
-        my_model = BaseModel()
         self.assertTrue(hasattr(my_model, 'id'))
         self.assertTrue(hasattr(my_model, 'created_at'))
         self.assertTrue(hasattr(my_model, 'updated_at'))
 
-    def test_str_representation(self):
+    def test_str(self):
         """
-        Test the string representation of BaseModel.
-        """
-        my_model = BaseModel()
-        self.assertEqual(str(my_model), "[BaseModel] ({}) {}"
-                         .format(my_model.id, my_model.__dict__))
-
-
-    def test_to_dict_method(self):
-        """
-        Test the to_dict method of BaseModel.
+        Test the string representation of the BaseModel.
         """
         my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
+        my_model_str = str(my_model)
+        self.assertTrue("[BaseModel]" in my_model_str)
+        self.assertTrue(my_model.id in my_model_str)
+
+    def test_save(self):
+        """
+        Test the save method of the BaseModel.
+        """
+        my_model = BaseModel()
+        initial_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(initial_updated_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        """
+        Test the to_dict method of the BaseModel.
+        """
+        my_model = BaseModel()
         my_model_dict = my_model.to_dict()
-        expected_keys = ['id', 'created_at',
-                         'updated_at', 'name', 'my_number', '__class__']
-        self.assertEqual(sorted(my_model_dict.keys()), sorted(expected_keys))
+        self.assertIsInstance(my_model_dict, dict)
         self.assertEqual(my_model_dict['__class__'], 'BaseModel')
-
-    def test_instance_creation_from_dict(self):
-        """ Test Instance creation from dict"""
-        data = {
-            'id': 'some_id',
-            'created_at': '2022-01-01T12:34:56.789000',
-            'updated_at': '2022-01-02T01:23:45.678000',
-            '__class__': 'BaseModel',
-            'additional_attribute': 'value'
-        }
-        new_instance = BaseModel(**data)
-        self.assertIsInstance(new_instance, BaseModel)
-        self.assertEqual(new_instance.id, 'some_id')
-        self.assertEqual(new_instance.additional_attribute, 'value')
-        self.assertIsInstance(new_instance.created_at, datetime)
-        self.assertIsInstance(new_instance.updated_at, datetime)
-
-    def test_to_dict_with_instance_from_dict(self):
-        """ Test to dict with instance from dict """
-        data = {
-            'id': 'some_id',
-            'created_at': '2022-01-01T12:34:56.789000',
-            'updated_at': '2022-01-02T01:23:45.678000',
-            '__class__': 'BaseModel',
-            'additional_attribute': 'value'
-        }
-        new_instance = BaseModel(**data)
-        new_instance_dict = new_instance.to_dict()
-        self.assertIn('additional_attribute', new_instance_dict)
+        self.assertEqual(my_model_dict['id'], my_model.id)
+        self.assertEqual(my_model_dict['created_at'],
+                         my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict['updated_at'],
+                         my_model.updated_at.isoformat())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,14 +3,11 @@
 Test cases for the FileStorage class.
 """
 
-import os
-import json
-import models
 import unittest
-
-from datetime import datetime
-from models.base_model import BaseModel
+import os
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestFileStorage(unittest.TestCase):
@@ -20,31 +17,59 @@ class TestFileStorage(unittest.TestCase):
 
     def setUp(self):
         """
-        Set up test resources.
+        Set up a clean environment for each test.
         """
-        self.file_path = 'file.json'
+        # Ensure that the test file does not exist initially
+        self.remove_test_file()
+        # Create a new FileStorage instance
         self.storage = FileStorage()
 
     def tearDown(self):
         """
-        Clean up test resources.
+        Clean up after each test.
         """
-        if os.path.exists(self.file_path):
-            os.remove(self.file_path)
+        # Remove the test file after each test
+        self.remove_test_file()
 
-    def test_new_instance(self):
+    def remove_test_file(self):
         """
-        Test creating a new instance of FileStorage.
+        Remove the test file if it exists.
         """
-        self.assertIsInstance(self.storage, FileStorage)
+        test_file_path = FileStorage._FileStorage__file_path
+        if os.path.exists(test_file_path):
+            os.remove(test_file_path)
 
     def test_all(self):
         """
-        Test the 'all' method returns a dictionary.
+        Test the all method of the FileStorage.
         """
-        all_data = self.storage.all()
-        self.assertIsInstance(all_data, dict)
+        all_objects = self.storage.all()
+        self.assertIsInstance(all_objects, dict)
+        self.assertEqual(all_objects, {})
+
+    def test_new(self):
+        """
+        Test the new method of the FileStorage.
+        """
+        my_model = BaseModel()
+        self.storage.new(my_model)
+        all_objects = self.storage.all()
+        self.assertEqual(len(all_objects), 1)
+        self.assertIn(f"BaseModel.{my_model.id}", all_objects)
+
+    def test_reload_nonexistent_file(self):
+        """
+        Test that reloading a nonexistent file does not raise an exception.
+        """
+        # Remove the test file before reloading
+        self.remove_test_file()
+
+        # Create a new FileStorage instance for reloading
+        new_storage = FileStorage()
+        new_storage.reload()
+
+        all_objects = new_storage.all()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
