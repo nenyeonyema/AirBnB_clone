8082cb84cd247db03988657dc2a8478ef615fbab
Slonjoh <tobson11122@gmail.com>
2023-12-12 06:50:00 -0800

    test user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,74 +1,38 @@
 #!/usr/bin/python3
 """
-Test module for the User class.
+Unittests for User class.
 """
-
 import unittest
-import models
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
-    """Test cases for the User class."""
+    """
+    Test cases for User class.
+    """
 
-    def setUp(self):
-        """Set up for the test."""
-        self.user = User()
-        self.user.first_name = "John"
-        self.user.last_name = "Doe"
-        self.user.email = "john.doe@example.com"
-        self.user.password = "password123"
-
-    def tearDown(self):
-        """Clean up after the test."""
-        del self.user
-
-    def test_instance(self):
-        """Test if user is an instance of User."""
-        self.assertIsInstance(self.user, User)
-
-    def test_attributes(self):
-        """Test if user has the required attributes."""
-        self.assertTrue(hasattr(self.user, 'email'))
-        self.assertTrue(hasattr(self.user, 'password'))
-        self.assertTrue(hasattr(self.user, 'first_name'))
-        self.assertTrue(hasattr(self.user, 'last_name'))
-
-    def test_attribute_types(self):
-        """Test if user attributes have the correct types."""
-        self.assertIsInstance(self.user.email, str)
-        self.assertIsInstance(self.user.password, str)
-        self.assertIsInstance(self.user.first_name, str)
-        self.assertIsInstance(self.user.last_name, str)
-
-    def test_save_method(self):
-        """Test if save method saves the instance to the storage."""
-        models.storage.reload()
-        initial_count = len(models.storage.all())
-        new_user = User()
-        new_user.email = "newuser@example.com"
-        new_user.password = "newpassword"
-        new_user.first_name = "New"
-        new_user.last_name = "User"
-
-    def test_to_dict_method(self):
+    def test_user_instance(self):
         """
-        Test if to_dict method returns a
-        dictionary with proper attributes.
+        Test creating an instance of User.
         """
-        user_dict = self.user.to_dict()
-        self.assertTrue('id' in user_dict)
-        self.assertTrue('created_at' in user_dict)
-        self.assertTrue('updated_at' in user_dict)
-        self.assertTrue('email' in user_dict)
-        self.assertTrue('password' in user_dict)
-        self.assertTrue('first_name' in user_dict)
-        self.assertTrue('last_name' in user_dict)
+        user = User()
+        self.assertIsInstance(user, User)
+        self.assertIsInstance(user, BaseModel)
 
-    def test_str_method(self):
-        """Test if the __str__ method returns a string representation."""
-        string = str(self.user)
-        self.assertIsInstance(string, str)
+    def test_user_attributes(self):
+        """
+        Test User attributes.
+        """
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
+        self.assertTrue(hasattr(user, 'password'))
+        self.assertTrue(hasattr(user, 'first_name'))
+        self.assertTrue(hasattr(user, 'last_name'))
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
 
 
 if __name__ == '__main__':
